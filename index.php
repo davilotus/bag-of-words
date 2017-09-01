c86b3ab5466a3d2a8108ece489965c3ebe8c91b9
nathanramorim <nathan.ramorim@gmail.com>
2017-09-01 01:05:00 -0300

    validando caso a > b

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
-$terms_1th = ['Gato','Palhaço','Botas'];
-$terms_2nd = ['Rei','Botas','Pato','Palhaço','Gato'];
+$terms_1th = ['Rei','Arroz','Pato','Palhaço','Gato'];
+$terms_2nd = ['Gato','Palhaço','Botas'];
 
 function check_bag($a,$b){
     $a_size = array_count_values($a);
     $b_size = array_count_values($b);
+
     if ($a_size > $b_size){
         $count = $a_size;
+        $score = 0;
         foreach ($b as $terms) {
-            var_dump($terms);
+           $score += (isset($count[$terms])) ? 1 : 0 ;
         }
+        var_dump($score);
     }else{
         $count = $b_size;
         $score = 0;
