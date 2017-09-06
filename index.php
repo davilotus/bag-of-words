720f7592a8b5f000116dd9655b83539bbc9a3060
nathanramorim <nathan.ramorim@gmail.com>
2017-09-06 20:34:51 -0300

    bag-of-words pronta, compara 2 arrays sem repetir valores iguais

--- index.php
+++ index.php
@@ -1,26 +1,63 @@
 <?php
+$_GET['d'] = NULL;
 $terms_1th = ['Rei','Arroz','Pato','Palhaço','Gato'];
-$terms_2nd = ['Gato','Palhaço','Botas'];
+$terms_3nd = ['Gato','Palhaço','Botas'];
+
+$terms_2nd = ['Rei','Arroz','Pato','Palhaço','Gato','Azul', 'Preto'];
+
+
+
 
 function check_bag($a,$b){
     $a_size = array_count_values($a);
     $b_size = array_count_values($b);
+    $arr = array_merge($a,$b);
+    $i = 0;
+    $a1 = NULL;
 
     if ($a_size > $b_size){
+        
         $count = $a_size;
         $score = 0;
-        foreach ($b as $terms) {
-           $score += (isset($count[$terms])) ? 1 : 0 ;
+
+        foreach ($a as $terms) {
+          if(isset($count[$terms])){
+            $a1[$terms] = $terms;
+          }
+          $i++;
         }
-        var_dump($score);
-    }else{
+        
+        foreach ($b as $terms) {
+          if(!isset($a1[$terms])){
+            $a1[$terms] = $terms;
+          }
+          $i++;
+        }
+        return $a1;
+    }
+    else{
+   
         $count = $b_size;
         $score = 0;
-        foreach ($a as $terms) {
-           $score += (isset($count[$terms])) ? 1 : 0 ;
+
+        foreach ($b as $terms) {
+          if(isset($count[$terms])){
+            $a1[$terms] = $terms;
+          }
+          $i++;
         }
-        var_dump($score);
+        
+        foreach ($a as $terms) {
+          if(!isset($a1[$terms])){
+            $a1[$terms] = $terms;
+          }
+          $i++;
+        }
+        return $a1;
     }
 }
 
-check_bag($terms_1th,$terms_2nd);
+
+
+$result = check_bag($terms_1th,$terms_3nd);
+var_dump($result).die();
