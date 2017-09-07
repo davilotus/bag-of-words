969ce93b4d4e4594e501fa3bcc6e38143fcb3633
nathanramorim <nathan.ramorim@gmail.com>
2017-09-06 21:49:21 -0300

    bootstrap 4 implementado

--- index.php
+++ index.php
@@ -8,15 +8,22 @@ $terms_3nd = ['Gato','Palhaço','Botas'];
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title></title>
+        <title>Bag Of Words | Flegas</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
+        <link href="css/bootstrap-grid.min.css" rel="stylesheet">
+        <link href="css/bootstrap-reboot.min.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
     </head>
     <body>
-        <?php $result = $words::get_bag($terms_1th,$terms_3nd)?>
-        <?=$words->bag_to_string($result)?>
+        <section class="container-fluid alert alert-info text-center">
+            <?php $result = $words::get_bag($terms_1th,$terms_3nd)?>
+            <?=$words->bag_to_string($result)?>    
+        </section>
     </body>
+    <script src="js/bootstrap.min.js"></script>
 </html>
 
 
